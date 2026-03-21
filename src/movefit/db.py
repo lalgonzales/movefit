59bb52a2fb6422b751d1c7d6dd99e6e3083ebc50
Luis Alfredo López Gonzales <lalgonzales@gmail.com>
2026-03-20 20:35:56 -0600

    feat(api): fix get_session for FastAPI session dependency

--- src/movefit/db.py
+++ src/movefit/db.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 from typing import Generator
 
 from sqlmodel import SQLModel, Session, create_engine
@@ -23,6 +24,7 @@ def create_db_and_tables(engine=None):
 
 
 def get_session() -> Generator[Session, None, None]:
+    """Yield a SQLModel session to use as FastAPI dependency."""
     engine = get_engine()
 
     with Session(engine) as session:
